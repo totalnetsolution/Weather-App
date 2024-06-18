df9387285f4b3d5f14b2f3fad6cb7b30f6a3c1aa
muhammad-danish <totalnetsolution2@gmail.com>
2024-06-18 13:47:05 +0500

    index file modified

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ form.addEventListener("submit", (event) => {
     cityInput.value = '';
   }
 });
+
+async function getWeatherData(city) {
+  try {
+    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city}&aqi=no`);
+    displayWeatherCard(response.data);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    alert('Please put correct name of city and try again.');
+  }
+}
