febe03916a2d629d9417ec19f34e84f4c7312b09
muhammad-danish <totalnetsolution2@gmail.com>
2024-06-18 13:44:48 +0500

    index file modified

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const form = document.querySelector("#form");
 const cityInput = document.querySelector("#city");
 const cardContainer = document.querySelector("#card-container");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const city = cityInput.value.trim();
+  if (city) {
+    getWeatherData(city);
+    cityInput.value = '';
+  }
+});
