066b72a1b18a4d4b85c681fed1df80be8d942789
muhammad-danish <totalnetsolution2@gmail.com>
2024-06-18 13:55:20 +0500

    index file modified

--- app.js
+++ app.js
@@ -25,3 +25,16 @@ function displayWeatherCard(data) {
   const weatherCard = document.createElement('div');
   weatherCard.classList.add('col-md-4', 'mb-4');
   weatherCard.innerHTML = `
+  <div class='bg-white border border-gray-800 shadow-lg ring ring-gray-700 ring-opacity-50 w-100 mx-auto mt-5 rounded-lg p-4 mb-5'>
+      <h1 class='text-2xl'>${data.location.name}</h1>
+      <p class='text-gray-500'>${data.location.localtime}, ${data.location.country}</p>
+      <div class='mt-5 d-flex justify-content-between align-items-center mx-5 pb-4'>
+        <h2 class='text-4xl md:text-7xl lg:text-8xl'>${data.current.temp_c}°C</h2>
+        <img width="160px" src=${data.current.condition.icon} alt='weatherImg' />
+      </div>
+      <h4>${data.current.condition.text}</h4>
+    </div>
+  `;
+  cardContainer.insertBefore(weatherCard, cardContainer.firstChild);
+}
+
